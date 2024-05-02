a1723299dddf6a4bedadc7afb3605e77c36bbc4f
Muhammed Malik Elhıdır <malekco721@gmail.com>
2024-05-02 12:03:10 +0300

    commit

---
+++ src/main/java/docker_projesi/docker_proje/DockerProjeApplication.java
@@ -0,0 +1,13 @@
+package docker_projesi.docker_proje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerProjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/docker_projesi/docker_proje/DockerProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package docker_projesi.docker_proje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
